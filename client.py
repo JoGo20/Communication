4f68593251f5cc7e457016ae10b61bbd61202773
=ezzatmohamed <=ezzat.mohamed2021@gmail.com>
2020-04-13 13:46:24 +0200

    completed

--- client.py
+++ client.py
@@ -2,6 +2,9 @@ import asyncio
 import json
 import websockets
 from enum import Enum
+import random
+import threading 
+import sys
 
 class States(Enum):
     INIT = 1
@@ -16,6 +19,9 @@ class Game:
         self.PlayerColor = None
         self.Socket = None
         self.Name = None
+    
+    def GetGameConfig(self):
+        return self.GameConfig
         
 GameInfo = Game()
 
@@ -25,19 +31,20 @@ async def ReadyState():
     
     response = await GameInfo.Socket.recv()
     response = json.loads(response)
-    # print(response['configuration']['initialState']['turn'])
     if(response['type'] == 'START'):
         # State = States.READY
         GameInfo.GameConfig = response['configuration']
         GameInfo.PlayerColor = response['color']
         print(GameInfo.GameConfig['initialState']['turn'])
         print(GameInfo.PlayerColor)
+        print(GameInfo.GameConfig['moveLog'])
+        MoveLog = GameInfo.GameConfig['moveLog']
         # print(GameInfo.PlayerColor == GameInfo.GameConfig['initialState']['turn'])
-        if GameInfo.PlayerColor == GameInfo.GameConfig['initialState']['turn']:
+        if ( (GameInfo.PlayerColor == GameInfo.GameConfig['initialState']['turn'] and len(MoveLog) % 2 == 0) or (GameInfo.PlayerColor != GameInfo.GameConfig['initialState']['turn'] and len(MoveLog) % 2 != 0)) :
             GameInfo.State = States.THINK
         else:
             GameInfo.State = States.IDLE
-        print(GameInfo.PlayerColor)
+        # print(GameInfo.PlayerColor)
     elif response['type'] == 'END':
         print("End")
     else:
@@ -62,23 +69,28 @@ async def IdleState():
 def MakeMove():
     PassMove = {'type' : 'pass'}
     ResignMove = {'type': 'resign'}
+    
+    x = random.randrange(0,20)
+    y = random.randrange(0,20)
+    
     PlaceMove = {
                     'type' : 'place',
                     'point': { 
-                                'row':-1,
-                                'column':-1 
+                                'row':x,
+                                'column':y
                             }
                  
                 }
     
-    return ResignMove
+    return PlaceMove
     
 
 async def ThinkState():
     global GameInfo
     print("Thinking")
-    while 1:
-        pass
+    # while 1:
+    #     print('x')
+    #     pass
     Msg = None
     while True:    
         # x =  input('Enter move')
@@ -86,6 +98,7 @@ async def ThinkState():
                         'type': 'MOVE',
                         'move': MakeMove()
                     }
+        
         print(MsgToSend)
         x = await GameInfo.Socket.send( json.dumps(MsgToSend) )
         
@@ -101,41 +114,51 @@ async def ThinkState():
     
     pass
 
-async def Pong():
-    global GameInfo
-    
-    while True :
-            GameInfo.Socket.pong()
-        
 
-async def InitState():
-    print("let's start!")
-    global GameInfo
-    GameInfo.Socket = await websockets.connect('ws://localhost:8080',ping_interval=1000) 
+async def InitState(name):
     
+    global GameInfo
+    GameInfo.Socket = await websockets.connect('ws://localhost:8080',ping_interval=100) 
+    print("let's start!")
+    t2 = threading.Thread(target=ping_pong_handler).start()  # ping pong
     
     response = await GameInfo.Socket.recv()
-    asyncio.get_event_loop().run_until_complete(Pong())
+    # asyncio.get_event_loop().run_until_complete(Pong())
     print(response)
     
-    x = await GameInfo.Socket.send(json.dumps({'type': 'NAME', 'name': 'Ezzat'}))   
+    x = await GameInfo.Socket.send(json.dumps({'type': 'NAME', 'name': str(name)}))   
     GameInfo.State = States.READY
-
-
-async def main():
+    
+async def ping_pong():
     global GameInfo
-    # print("Welcome !")
+    print("Ping")
+    while True:
+        try:
+            await GameInfo.Socket.pong()
+            await asyncio.sleep(0.5)
+        except Exception as e:
+            print(e)
+            return
+
+def ping_pong_handler():
+    asyncio.new_event_loop().run_until_complete(ping_pong())
+
+async def main(name):
+    global GameInfo
     while(1):
-        print(GameInfo.State)
-        if GameInfo.State == States.INIT:
-            await InitState()
-        elif  GameInfo.State == States.READY:
-            await ReadyState()
-        elif  GameInfo.State == States.IDLE:
-            await IdleState()
-        elif  GameInfo.State == States.THINK:
-            await ThinkState()
+        try:
+            if GameInfo.State == States.INIT:
+                await InitState(name)
+            elif  GameInfo.State == States.READY:
+                await ReadyState()
+            elif  GameInfo.State == States.IDLE:
+                await IdleState()
+            elif  GameInfo.State == States.THINK:
+                await ThinkState()
+        except:
+            GameInfo.State = States.INIT
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    name = sys.argv[1]
+    asyncio.get_event_loop().run_until_complete(main(name))
     
